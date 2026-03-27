345ff76a8a217f26c3428fef5f2f481d05bd72ac
CoslaJohn <consul@cosla.gov.uk>
2026-03-27 11:54:46 +0000

    Add rack attack for rate limiting

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,27 @@
+class Rack::Attack
+  # Use existing Memcached store (from your production.rb)
+  Rack::Attack.cache.store = Rails.cache
+
+  # Always allow requests from localhost
+  safelist('allow from localhost') do |req|
+    '127.0.0.1' == req.ip || '::1' == req.ip
+  end
+
+  # If any single IP makes 300 requests in 5 minutes, they are likely a bad scraper.
+  throttle('req/ip', limit: 300, period: 5.minutes) do |req|
+    req.ip unless req.path.start_with?('/assets')
+  end
+
+  # Throttling login attempts by IP (6 reqs/minute)
+  throttle('logins/ip', limit: 6, period: 60.seconds) do |req|
+    if req.path == '/users/sign_in' && req.post?
+      req.ip
+    end
+  end
+
+  # This allows you to see blocks in your Rails logs
+  ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |name, start, finish, request_id, payload|
+    req = payload[:request]
+    Rails.logger.warn "[Rack::Attack] Throttled: #{req.ip} path: #{req.path} (Rule: #{req.env['rack.attack.matched']})"
+  end
+end
